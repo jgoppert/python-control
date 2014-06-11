b9099b16cdbb087e8d5c7cef4b6738af07770c5f
James Goppert <james.goppert@gmail.com>
2014-06-11 10:35:36 -0400

    Simplified build.

--- setup.py
+++ setup.py
@@ -23,7 +23,11 @@ with upstream soon.
 
 """
 
-from __future__ import division, print_function
+MAJOR = 0
+MINOR = 6
+MICRO = 2
+ISRELEASED = True
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 DOCLINES = __doc__.split("\n")
 
@@ -31,15 +35,7 @@ import os
 import sys
 import subprocess
 
-
-if sys.version_info[:2] < (2, 6) or (3, 0) <= sys.version_info[0:2] < (3, 2):
-    raise RuntimeError("Python version 2.6, 2.7 or >= 3.2 required.")
-
-if sys.version_info[0] >= 3:
-    import builtins
-else:
-    import __builtin__ as builtins
-
+from setuptools import setup, find_packages
 
 CLASSIFIERS = """\
 Development Status :: 3 - Alpha
@@ -56,12 +52,6 @@ Operating System :: POSIX
 Operating System :: Unix
 Operating System :: MacOS
 """
-
-MAJOR = 0
-MINOR = 6
-MICRO = 2
-ISRELEASED = True
-VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 
 # Return the git revision as a string
@@ -91,21 +81,10 @@ def git_version():
 
     return GIT_REVISION
 
-# BEFORE importing distutils, remove MANIFEST. distutils doesn't properly
-# update it when the contents of directories change.
-if os.path.exists('MANIFEST'):
-    os.remove('MANIFEST')
-
-# This is a bit hackish: we are setting a global variable so that the main
-# control __init__ can detect if it is being loaded by the setup routine, to
-# avoid attempting to load components that aren't built yet.  While ugly, it's
-# a lot more robust than what was previously being used.
-builtins.__CONTROL_SETUP__ = True
-
 
 def get_version_info():
     # Adding the git rev number needs to be done inside write_version_py(),
-    # otherwise the import of control.version messes up
+    # otherwise the import of package.version messes up
     # the build under Python 3.
     FULLVERSION = VERSION
     if os.path.exists('.git'):
@@ -129,7 +108,7 @@ def get_version_info():
 
 def write_version_py(filename='control/version.py'):
     cnt = """
-# THIS FILE IS GENERATED FROM CONTROL SETUP.PY
+# THIS FILE IS GENERATED FROM SETUP.PY
 short_version = '%(version)s'
 version = '%(version)s'
 full_version = '%(full_version)s'
@@ -149,50 +128,6 @@ if not release:
                        'isrelease': str(ISRELEASED)})
     finally:
         a.close()
-
-
-def configuration(parent_package='', top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration(None, parent_package, top_path)
-    config.set_options(ignore_setup_xxx_py=True,
-                       assume_default_configuration=True,
-                       delegate_options_to_subpackages=True,
-                       quiet=True)
-    config.add_subpackage('control')
-    config.get_version('control/version.py')  # sets config.version
-    return config
-
-
-def check_submodules():
-    """ verify that the submodules are checked out and clean
-        use `git submodule update --init`; on failure
-    """
-    if not os.path.exists('.git'):
-        return
-    with open('.gitmodules') as f:
-        for l in f:
-            if 'path' in l:
-                p = l.split('=')[-1].strip()
-                if not os.path.exists(p):
-                    raise ValueError('Submodule %s missing' % p)
-
-    proc = subprocess.Popen(['git', 'submodule', 'status'],
-                            stdout=subprocess.PIPE)
-    status, _ = proc.communicate()
-    status = status.decode("ascii", "replace")
-    for line in status.splitlines():
-        if line.startswith('-') or line.startswith('+'):
-            raise ValueError('Submodule not clean: %s' % line)
-
-from distutils.command.sdist import sdist
-
-
-class sdist_checked(sdist):
-    """ check submodules on sdist to prevent incomplete tarballs """
-    def run(self):
-        # control had no submodules currently
-        # check_submodules()
-        sdist.run(self)
 
 
 def setup_package():
@@ -217,33 +152,18 @@ def setup_package():
         license='BSD',
         classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
         platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
-        cmdclass={"sdist": sdist_checked},
         requires=['numpy', 'scipy'],
+        install_requires=['parsimonious', 'ply', 'grako', 'enum'],
+        tests_require=['nose'],
+        test_suite='nose.collector',
+        packages=find_packages(
+            # choosing to distribute tests
+            # exclude=['*.test']
+        ),
     )
 
-    # Run build
-    if len(sys.argv) >= 2 and \
-            ('--help' in sys.argv[1:] or
-             sys.argv[1] in ('--help-commands', 'egg_info', '--version',
-             'clean')):
-        # Use setuptools for these commands (they don't work well or at all
-        # with distutils).  For normal builds use distutils.
-        try:
-            from setuptools import setup
-        except ImportError:
-            from distutils.core import setup
-
-        FULLVERSION, GIT_REVISION = get_version_info()
-        metadata['version'] = FULLVERSION
-    elif len(sys.argv) >= 2 and sys.argv[1] == 'bdist_wheel':
-        # bdist_wheel needs setuptools
-        import setuptools
-        setuptools  # reference once for pyflakes
-        from numpy.distutils.core import setup
-        metadata['configuration'] = configuration
-    else:
-        from numpy.distutils.core import setup
-        metadata['configuration'] = configuration
+    FULLVERSION, GIT_REVISION = get_version_info()
+    metadata['version'] = FULLVERSION
 
     try:
         setup(**metadata)
@@ -251,7 +171,6 @@ def setup_package():
         del sys.path[0]
         os.chdir(old_path)
     return
-
 
 if __name__ == '__main__':
     setup_package()
